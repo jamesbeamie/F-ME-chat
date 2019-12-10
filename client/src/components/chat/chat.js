85b624ea4234ee0e0c1acccb173bf409b65a072e
James <admin@James.local>
2019-12-10 20:16:33 +0300

    emiting sockets

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const endPoint = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log(location.search);
+    console.log(name, room);
+
+    socket = io(endPoint);
+    console.log("@@@@@@", socket);
+
+    setName(name);
+    setRoom(room);
+    //Emitting a specific event on hooks
+
+    socket.emit("join", { name, room });
+  }, [endPoint, useEffect]);
+
   return <h1>Chat</h1>;
 };
 export default Chat;
