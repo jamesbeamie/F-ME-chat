24b19d97f0052e2d2816046faad5ced6368d4358
James <admin@James.local>
2019-12-17 22:08:53 +0300

    user helpers

---
+++ server/users.js
@@ -0,0 +1,38 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+
+  const userExists = users.find(
+    user => user.name === name && user.room === room
+  );
+  if (userExists) {
+    return { error: "username already taken" };
+  }
+
+  const newUser = { id, name, room };
+
+  users.push(newUser);
+  return newUser;
+};
+
+const getUser = id => {
+  const theUser = users.find(user => user.id === id);
+  return theUser;
+};
+
+const removeUser = id => {
+  const findUser = users.findIndex(user => user.id === id);
+
+  if (findUser !== -1) {
+    return users.splice(findUser, 1)[0];
+  }
+};
+
+const getUsersInSpecificRoom = room => {
+  const usersInRoom = users.filter(user => user.room === room);
+  return usersInRoom;
+};
+
+module.exports = { addUser, removeUser, getUser, getUsersInSpecificRoom };
