6b226a4a5bf4c4790379a0809e1f728bd6c70846
James <admin@James.local>
2019-12-20 23:05:49 +0300

    Removed user to retaion message after refresh

--- client/src/components/chat/chat.js
+++ client/src/components/chat/chat.js
@@ -59,12 +59,12 @@ const Chat = ({ location }) => {
     <div className='outerContainer'>
       <div className='container'>
         <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
         <InputComponent
           message={message}
           setMessage={setMessage}
           sendMessage={sendMessage}
         />
-        <Messages messages={messages} name={name} />
       </div>
     </div>
   );
